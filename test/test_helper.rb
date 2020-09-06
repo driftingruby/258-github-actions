2018d4717df108ce10c9bd5d3e5836f4ec1d1cdb
kobaltz <dave@k-innovations.net>
2020-09-05 22:51:42 -0400

    setup ci for rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
